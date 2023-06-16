e81b5d2944ff20332d961118bdd5f504bb70d8bf
Vishal Rathore <vishal.rathore@multitech.com>
2023-06-15 21:05:09 -0500

    updated libraries and react minor versions

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,18 @@ function App(): ReactElement {
   return (
     <>
       <div>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={(): void => setCount(count => count + 1)}>
-          count is {count}
-        </button>
+      <div className="card">
+        <button onClick={(): void => setCount(count => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
